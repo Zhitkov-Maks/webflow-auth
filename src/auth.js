8eca33081bd0b037143d83ff0d9b243acce700bd
Maks Z <m-zhitkov@inbox.ru>
2025-12-18 11:33:23 +0300

    Implement login function

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,9 @@
 // Authentication module
 function login(username, password) {
-    // TODO: Implement
+    if (checkCredentals(username, password)) {
+        return { success: true, token: 'abc123' };
+    }
+    return { success: false };
 }
 
 function checkCredentials(username, password) {  // опечатка в названии
