f3959f50acce4984fbbeccb2972cd0ab65b42ebc
Maks Z <m-zhitkov@inbox.ru>
2025-12-18 11:34:48 +0300

    Add password validation

--- src/validation.js
+++ src/validation.js
@@ -5,6 +5,9 @@ function validateEmail(email) {
 
 function sanitizeInput(input) {
     return input.replace(/[<>]/g, '');  // защита от XSS
+
+function validatePassword(password) {
+    return password.length >= 8;
 }
 
 module.exports = { validateEmail };
